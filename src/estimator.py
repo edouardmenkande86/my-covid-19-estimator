7c81212610407c4f6425a3375e1efb3f228db483
unknown <edouardmenkande@yahoo.fr>
2020-04-12 21:52:33 +0100

    start building estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,3 +1,5 @@
+import math
+
 def estimator(data):
     data = {
         'region': {
@@ -18,56 +20,112 @@ def estimator(data):
 # challenge 1
 
 def covid19ImpactEstimator(data):
+
     estimate = {
-        'impact':{
-            'currentlyInfected':data['reportedCases'] * 10
+        'impact': {
+            'currentlyInfected': data.get('reportedCases') * 10
         },
         'severeImpact': {
-            'currentlyInfected': data['reportedCases'] * 50
+            'currentlyInfected': data.get('reportedCases') * 50
         }
     }
     if data['periodeType'] == 'days':
+        periodTime = data.get('timeToElapse')
+
         estimate = {
             'impact': {
-                'currentlyInfected': data['reportedCases'] * 10,
-                'infectionsByRequestedTime': estimate.get('impact').get('currentlyInfected') * 18
+                'currentlyInfected': data.get('reportedCases') * 10,
+                'infectionsByRequestedTime': estimate.get('impact').get('currentlyInfected') * 18,
+                'severeCasesByRequestedTime': estimate.get('impact').get('infectionByRequestedTime') * 0.15,
+                'hospitalBedsByRequestedTime': data.get('totalHospitalBeds')*0.35 - estimate.get('impact').get(
+                    'severeCasesByRequestedTime'),
+                'casesForICUByRequestedTime': estimate.get('impact').get('infectionsByRequestedTime') * 0.05,
+                'casesForVentilatorsByRequestedTime': estimate.get('impact').get('infectionsByRequestedTime') * 0.02,
+                'dollarsInFlight': math.trunc((estimate.get('infectionsByRequestedTime')
+                                               * data.get('region').get('avgDailyIncomePopulation')
+                                               * data.get('region').get)/periodTime)
+
             },
             'severeImpact': {
-                'currentlyInfected': data['reportedCases'] * 50,
-                'infectionsByRequestedTime': estimate.get('severeImpact').get('currentlyInfected') * 18
+                'currentlyInfected': data.get('reportedCases') * 50,
+                'infectionsByRequestedTime': estimate.get('severeImpact').get('currentlyInfected') * 18,
+                'severeCasesByRequestedTime': estimate.get('severeImpact').get('infectionByRequestedTime') * 0.15,
+                'hospitalBedsByRequestedTime': data.get('totalHospitalBeds')*0.35 - estimate.get('severeImpact').get(
+                    'severeCasesByRequestedTime'),
+                'casesForICUByRequestedTime': estimate.get('severeImpact').get('infectionsByRequestedTime') * 0.05,
+                'casesForVentilatorsByRequestedTime': estimate.get('severeImpact').get('infectionsByRequestedTime') * 0.02,
+                'dollarsInFlight': math.trunc((estimate.get('infectionsByRequestedTime')
+                                               * data.get('region').get('avgDailyIncomePopulation')
+                                               * data.get('region').get) / periodTime)
             }
 
         }
     elif data['periodeType'] == 'weeks':
+        periodTime = data.get('timeToElapse')*7
+
         estimate = {
             'impact': {
-                'currentlyInfected': data['reportedCases'] * 10,
-                'infectionsByResquestedTime': estimate.get('impact').get('currentlyInfected') * 137
+                'currentlyInfected': data.get('reportedCases') * 10,
+                'infectionsByResquestedTime': estimate.get('impact').get('currentlyInfected') * 137,
+                'severeCasesByRequestedTime': estimate.get('impact').get('infectionByRequestedTime') * 0.15,
+                'hospitalBedsByRequestedTime': data.get('totalHospitalBeds')*0.35 - estimate.get('impact').get(
+                    'severeCasesByRequestedTime'),
+                'casesForICUByRequestedTime': estimate.get('impact').get('infectionsByRequestedTime') * 0.05,
+                'casesForVentilatorsByRequestedTime': estimate.get('impact').get('infectionsByRequestedTime') * 0.02,
+                'dollarsInFlight': math.trunc((estimate.get('infectionsByRequestedTime')
+                                               * data.get('region').get('avgDailyIncomePopulation')
+                                               * data.get('region').get) / periodTime)
+
             },
             'severeImpact': {
-                'currentlyInfected': data['reportedCases'] * 50,
-                'infectionsByResquetedTime': estimate.get('severeImpact').get('currentlyInfected') * 137
+                'currentlyInfected': data.get('reportedCases') * 50,
+                'infectionsByResquetedTime': estimate.get('severeImpact').get('currentlyInfected') * 137,
+                'severeCasesByRequestedTime': estimate.get('severeImpact').get('infectionByRequestedTime') * 0.15,
+                'hospitalBedsByRequestedTime': data.get('totalHospitalBeds')*0.35 - estimate.get('severeImpact').get(
+                    'severeCasesByRequestedTime'),
+                'casesForICUByRequestedTime': estimate.get('severeImpact').get('infectionsByRequestedTime') * 0.05,
+                'casesForVentilatorsByRequestedTime': estimate.get('severeImpact').get('infectionsByRequestedTime') * 0.02,
+                'dollarsInFlight': math.trunc((estimate.get('infectionsByRequestedTime')
+                                               * data.get('region').get('avgDailyIncomePopulation')
+                                               * data.get('region').get) / periodTime)
             }
         }
 
     elif data['periodeType'] == 'months':
+        periodTime = data.get('timeToElapse') * 30
+
         estimate = {
             'impact': {
-                'currentlyInfected': data['reportedCases'] * 10,
-                'infectionsByRequestedTime': estimate.get('impact').get('currentlyInfected') * 548
+                'currentlyInfected': data.get('reportedCases') * 10,
+                'infectionsByRequestedTime': estimate.get('impact').get('currentlyInfected') * 548,
+                'severeCasesByRequestedTime': estimate.get('impact').get('infectionByRequestedTime') * 0.15,
+                'hospitalBedsByRequestedTime': data.get('totalHospitalBeds')*0.35 - estimate.get('impact').get(
+                    'severeCasesByRequestedTime'),
+                'casesForICUByRequestedTime': estimate.get('impact').get('infectionsByRequestedTime') * 0.05,
+                'casesForVentilatorsByRequestedTime': estimate.get('impact').get('infectionsByRequestedTime') * 0.02,
+                'dollarsInFlight': math.trunc((estimate.get('infectionsByRequestedTime')
+                                               * data.get('region').get('avgDailyIncomePopulation')
+                                               * data.get('region').get) / periodTime)
             },
             'severeImpact': {
                 'currentlyInfected': data['reportedCases'] * 50,
-                'infectionsByResquetedTime': estimate.get('severeImpact').get('currentlyInfected') * 548
+                'infectionsByResquetedTime': estimate.get('severeImpact').get('currentlyInfected') * 548,
+                'severeCasesByRequestedTime': estimate.get('severeImpact').get('infectionByRequestedTime') * 0.15,
+                'hospitalBedsByRequestedTime': data.get('totalHospitalBeds')*0.35 - estimate.get('severeImpact').get(
+                    'severeCasesByRequestedTime'),
+                'casesForICUByRequestedTime': estimate.get('severeImpact').get('infectionsByRequestedTime') * 0.05,
+                'casesForVentilatorsByRequestedTime': estimate.get('severeImpact').get('infectionsByRequestedTime') * 0.02,
+                'dollarsInFlight': math.trunc((estimate.get('infectionsByRequestedTime')
+                                               * data.get('region').get('avgDailyIncomePopulation')
+                                               * data.get('region').get) / periodTime)
             }
         }
 
     else:
         print("Choose the valid periode Type.")
 
+    return { data:{}, estimate['impact']:{}, estimate['severeImpact']:{}}
 
-
-    return data
 
   
 
